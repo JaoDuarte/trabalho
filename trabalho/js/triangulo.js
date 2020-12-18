5c4f0c4a290967f485ecde50fcd1003eaa424c82
JaoDuarte <71887974+JaoDuarte@users.noreply.github.com>
2020-12-18 10:24:08 -0300

    Add files via upload

---
+++ trabalho/js/triangulo.js
@@ -0,0 +1,21 @@
+var res1 = "Equilatero"
+    var res2 = "Isosceles"
+    var res3 = "Escaleno"
+
+    function triangulo() {
+        let la = document.getElementById("la").value
+        let lb = document.getElementById("lb").value
+        let lc = document.getElementById("lc").value
+
+        if (la == lb || la == lc) {
+            document.getElementById("resultado2").textContent = res1
+        }
+
+        else if (la == lb || la == lc || lb == lc) {
+            document.getElementById("resultado2").textContent = res2
+        }
+
+        else {
+            document.getElementById("resultado2").textContent = res3
+        }
+    }
